615d9e3141f214d2181f94015117c1fef3488f40
Jukka Svahn <web@rahinaa.biz>
2013-04-29 12:26:24 +0300

    Added a base class.

---
+++ Plugin/Base.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Rah\TextpatternPluginInstaller\Plugin;
+use Rah\TextpatternPluginInstaller\Textpattern\Find as Textpattern;
+
+/**
+ * Process the manifest configuration.
+ */
+
+abstract class Base
+{
+    /**
+     * Path to the plugin directory.
+     *
+     * @var string
+     */
+ 
+    protected $dir;
+
+    /**
+     * Stores the plugin contents.
+     *
+     * @var Package
+     */
+
+    protected $plugin;
+
+    /**
+     * Packaged plugin installer.
+     *
+     * @var string
+     */
+
+    protected $package;
+
+    /**
+     * Constructor.
+     */
+
+    public function __construct($directory)
+    {
+        if ($manifest = $this->find($directory))
+        {
+            $this->dir = dirname($manifest);
+            $this->import();
+            $this->package();
+        }
+        else
+        {
+            throw new \InvalidArgumentException('The plugin directory was not found.');
+        }
+    }
+
+    /**
+     * Lists the package contents.
+     *
+     * @param  string                         $directory
+     * @return RecursiveIteratorIterator|bool
+     */
+
+    protected function find($directory)
+    {
+        if (!file_exists($directory) || !is_dir($directory) || !is_readable($directory))
+        {
+            return false;
+        }
+
+        $iterator = new \RecursiveDirectoryIterator(realpath($directory));
+        return new \RecursiveIteratorIterator($iterator);
+    }
+
+    /**
+     * Packages the plugin data.
+     */
+
+    protected function package()
+    {
+        $this->package = base64_encode(gzencode(serialize((array) $this->plugin)));
+    }
+
+    /**
+     * Installs a plugin.
+     */
+
+    public function install()
+    {
+        $_POST['plugin64'] = $this->package;
+        ob_start();
+        plugin_install();
+        ob_end_clean();
+    }
+
+    /**
+     * Updates a plugin.
+     */
+
+    public function update()
+    {
+        $this->install();
+    }
+
+    /**
+     * Uninstalls a plugin.
+     */
+
+    public function uninstall()
+    {
+        $_POST['selected'] = array($this->plugin->name);
+        $_POST['edit_method'] = 'delete';
+        ob_start();
+        plugin_multi_edit();
+        ob_end_clean();
+    }
+
+    /**
+     * Forms absolute file path.
+     *
+     * @param  string $path
+     * @return string
+     */
+
+    protected function path($path)
+    {
+        if (strpos($path, './') === 0)
+        {
+            return $this->dir . '/' . substr($path, 2);
+        }
+
+        if (strpos($path, '../') === 0)
+        {
+            return dirname($this->dir) . '/' . substr($path, 3);
+        }
+
+        return $path;
+    }
+}
--- Plugin/Manifest.php
+++ Plugin/Manifest.php
@@ -7,16 +7,8 @@ use Rah\TextpatternPluginInstaller\Textpattern\Find as Textpattern;
  * Process the manifest configuration.
  */
 
-class Manifest
+class Manifest extends Base
 {
-    /**
-     * Path to the plugin directory.
-     *
-     * @var string
-     */
- 
-    protected $dir;
-
     /**
      * Stores manifest files contents.
      *
@@ -24,40 +16,6 @@ class Manifest
      */
 
     protected $manifest;
-
-    /**
-     * Stores the plugin contents.
-     *
-     * @var Package
-     */
-
-    protected $plugin;
-
-    /**
-     * Package plugin installer.
-     *
-     * @var string
-     */
-
-    protected $package;
-
-    /**
-     * Constructor.
-     */
-
-    public function __construct($directory)
-    {
-        if ($manifest = $this->find($directory))
-        {
-            $this->dir = dirname($manifest);
-            $this->import();
-            $this->package();
-        }
-        else
-        {
-            throw new \InvalidArgumentException('The manifest.json was not found.');
-        }
-    }
 
     /**
      * Find the plugin project directory and manifest from the package.
@@ -68,21 +26,16 @@ class Manifest
 
     protected function find($directory)
     {
-        if (!file_exists($directory) || !is_dir($directory) || !is_readable($directory))
+        if ($iterator = parent::find($directory))
         {
-            return false;
-        }
-
-        $iterator = new \RecursiveDirectoryIterator(realpath($directory));
-        $iterator = new \RecursiveIteratorIterator($iterator);
-
-        foreach ($iterator as $file)
-        {
-            if (basename($file) === 'manifest.json' && is_file($file) && is_readable($file) && $contents = file_get_contents($file))
+            foreach ($iterator as $file)
             {
-                if (($this->manifest = @json_decode($contents)) && isset($this->manifest->name) && $this->manifest->name === basename(dirname($file)))
+                if (basename($file) === 'manifest.json' && is_file($file) && is_readable($file) && $contents = file_get_contents($file))
                 {
-                    return $file;
+                    if (($this->manifest = @json_decode($contents)) && isset($this->manifest->name) && $this->manifest->name === basename(dirname($file)))
+                    {
+                        return $file;
+                    }
                 }
             }
         }
@@ -112,49 +65,6 @@ class Manifest
         $this->plugin->help = '';
         $this->plugin->help_raw = $this->help();
         $this->plugin->textpack = $this->textpack();
-    }
-
-    /**
-     * Packages the plugin data.
-     */
-
-    protected function package()
-    {
-        $this->package = base64_encode(gzencode(serialize((array) $this->plugin)));
-    }
-
-    /**
-     * Installs a plugin.
-     */
-
-    public function install()
-    {
-        $_POST['plugin64'] = $this->package;
-        ob_start();
-        plugin_install();
-        ob_end_clean();
-    }
-
-    /**
-     * Updates a plugin.
-     */
-
-    public function update()
-    {
-        $this->install();
-    }
-
-    /**
-     * Uninstalls a plugin.
-     */
-
-    public function uninstall()
-    {
-        $_POST['selected'] = array($this->manifest->name);
-        $_POST['edit_method'] = 'delete';
-        ob_start();
-        plugin_multi_edit();
-        ob_end_clean();
     }
 
     /**
@@ -223,7 +133,7 @@ class Manifest
     }
 
     /**
-     * Processes help files.
+     * Gets help files.
      *
      * @return string
      */
@@ -245,27 +155,5 @@ class Manifest
         }
 
         return implode(n, $out);
-    }
-
-    /**
-     * Forms absolute file path.
-     *
-     * @param  string $path
-     * @return string
-     */
-
-    protected function path($path)
-    {
-        if (strpos($path, './') === 0)
-        {
-            return $this->dir . '/' . substr($path, 2);
-        }
-
-        if (strpos($path, '../') === 0)
-        {
-            return dirname($this->dir) . '/' . substr($path, 3);
-        }
-
-        return $path;
     }
 }
