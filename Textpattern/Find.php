5f7687905391b2a04c6c935a44d2eddaa85fb6df
Jukka Svahn <web@rahinaa.biz>
2013-05-04 17:33:19 +0300

    Renamed repository name in header comments.

--- Textpattern/Find.php
+++ Textpattern/Find.php
@@ -1,24 +1,24 @@
 <?php
 
 /*
- * Textpattern Plugin Installer for Composer
+ * Textpattern Installer for Composer
  * https://github.com/gocom/textpattern-installer
  *
  * Copyright (C) 2013 Jukka Svahn
  *
- * This file is part of Textpattern Plugin Installer.
+ * This file is part of Textpattern Installer.
  *
- * Textpattern Plugin Installer is free software; you can redistribute
+ * Textpattern Installer is free software; you can redistribute
  * it and/or modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation, version 2.
  *
- * Textpattern Plugin Installer is distributed in the hope that it
+ * Textpattern Installer is distributed in the hope that it
  * will be useful, but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Textpattern Plugin Installer. If not, see
+ * along with Textpattern Installer. If not, see
  * <http://www.gnu.org/licenses/>.
  */
 
