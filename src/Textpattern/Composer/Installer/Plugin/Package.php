63b6a2cb53bdecbe293124407b639ad956509965
Jukka Svahn <void@rah.pw>
2019-03-16 15:41:45 +0200

    Missing dates

--- src/Textpattern/Composer/Installer/Plugin/Package.php
+++ src/Textpattern/Composer/Installer/Plugin/Package.php
@@ -4,7 +4,7 @@
  * Textpattern Installer for Composer
  * https://github.com/gocom/textpattern-installer
  *
- * Copyright (C) 2013 Jukka Svahn
+ * Copyright (C) 2019 Jukka Svahn
  *
  * This file is part of Textpattern Installer.
  *
